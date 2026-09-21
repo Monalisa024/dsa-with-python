3ab6ad4d338b9a27775b4d3ca52cf6d9c796f88f
Monalisa Sahoo <sahoomonalisa052@gmail.com>
2026-09-21 20:52:08 +0530

    Time: 4 ms (12.24%), Space: 19.2 MB (65.52%) - LeetHub

---
+++ 3046-split-the-array/3046-split-the-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPossibleToSplit(self, nums):
+        for num in set(nums):
+            if nums.count(num) > 2:
+                return False
+        return True
+        
